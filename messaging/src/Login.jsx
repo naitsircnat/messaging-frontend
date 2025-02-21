758eac065bc01890671f0ad6a73ba225efc1c22a
naitsircnat <cristiangerardchen@gmail.com>
2025-02-21 08:41:25 +0000

    Added formik form for login and connected to backend, without setting setJwt

--- messaging/src/Login.jsx
+++ messaging/src/Login.jsx
@@ -1,35 +1,90 @@
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useLocation } from "wouter";
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+});
+
 export default function Login() {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const [, setLocation] = useLocation();
+
+  const handleSubmit = async (values, formikHelpers) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/user/login`,
+        values
+      );
+
+      console.log("Log in successful: ", response.data);
+
+      setLocation("/");
+    } catch (error) {
+      console.error("Log in failed", error.response?.data || error.message);
+    } finally {
+      formikHelpers.setSubmitting(false);
+    }
+  };
+
   return (
-    <>
-      <div className="container my-4">
-        <h1>Login</h1>
-        <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    </>
+    <div className="container my-5">
+      <h1>Login</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <Field
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+              />
+              {formik.errors.email && formik.touched.email ? (
+                <div className="text-danger">{formik.errors.email}</div>
+              ) : null}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <Field
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+              />
+              {formik.errors.password && formik.touched.password ? (
+                <div className="text-danger">{formik.errors.password}</div>
+              ) : null}
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={formik.isSubmitting}
+            >
+              Log In
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
