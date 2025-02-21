6e1a2f15cb5db4100a71e49311919efb1f1a987b
naitsircnat <cristiangerardchen@gmail.com>
2025-02-21 06:33:46 +0000

    Added content for login and register pages

--- messaging/src/Login.jsx
+++ messaging/src/Login.jsx
@@ -1,7 +1,35 @@
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="container my-4">
+        <h1>Login</h1>
+        <form>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
